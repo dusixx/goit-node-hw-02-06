c2821959cf565ad7c2608594ab97a938f611bb95
dusixx <113414935+dusixx@users.noreply.github.com>
2023-08-16 10:59:18 +0300

    small fix

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -8,18 +8,19 @@ import {
   STATIC_PATH,
 } from '../../constants/index.js';
 
+const { path: avatarsPath } = AVATAR_OPTIONS;
+
 export const updateAvatar = async ({ user, file }, res) => {
   if (!file) {
     throw HttpError(HTTP_STATUS.badRequest, 'avatar: need a file');
   }
-
   const { avatarUrl } = file;
   const { avatarUrl: oldAvatarUrl } = await User.findByIdAndUpdate(user._id, {
     avatarUrl,
   });
 
   // удаляем старый аватар, если это не gravatar-ссылка
-  if (oldAvatarUrl.startsWith(AVATAR_OPTIONS.path)) {
+  if (oldAvatarUrl.startsWith(avatarsPath)) {
     const fullName = path.resolve(STATIC_PATH, oldAvatarUrl);
     try {
       await checkFileExists(fullName);
